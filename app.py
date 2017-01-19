e691ae577288df53561c5ca0ce49ff9b50bf4eb6
ashleycoxley <ashleycoxley@gmail.com>
2017-01-18 22:40:27 -0800

    feat: Instructor page content

--- app.py
+++ app.py
@@ -10,6 +10,31 @@ def main_page():
     return flask.render_template('main.html')
 
 
+@app.route('/lessons')
+def lesson_page():
+    return flask.render_template('lessons.html')
+
+
+@app.route('/boarding')
+def boarding_page():
+    return flask.render_template('boarding.html')
+
+
+@app.route('/training')
+def training_page():
+    return flask.render_template('training.html')
+
+
+@app.route('/instructors')
+def instructors_page():
+    return flask.render_template('instructors.html')
+
+
+@app.route('/contact', methods=['GET'])
+def contact_page():
+    return flask.render_template('contact.html')
+
+
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.run(host='0.0.0.0', port=8080, debug=True)
