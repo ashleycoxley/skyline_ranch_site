cd771e1ec137822ccf4b0ea353b5bc6f3c46ff03
ashleycoxley <ashleycoxley@gmail.com>
2017-02-28 13:19:03 -0800

    feat: Page content
    Updated page content and images

--- app.py
+++ app.py
@@ -1,48 +1,84 @@
 import flask
-import sqlalchemy
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, validators
+import contact_form
+import os
+import json
 
 app = flask.Flask(__name__)
+IMG_EXTENSIONS = ['png', 'jpeg', 'jpg']
+MAIN_PAGE_CONTENT = 'main_page_content.json'
+
+
+class MessageForm(FlaskForm):
+    name = StringField('name', validators=[validators.DataRequired()])
+    email = StringField('email', validators=[validators.DataRequired(), validators.Email()])
+    subject = StringField('subject', validators=[validators.DataRequired()])
+    message = TextAreaField('message', validators=[validators.DataRequired()])
 
 
 @app.route('/')
 def main_page():
-    return flask.render_template('main.html')
+    with open(MAIN_PAGE_CONTENT, 'r') as infile:
+        content = json.load(infile).get('carousel-content')
+    return flask.render_template('main.html',
+                                 content=content)
 
 
 @app.route('/lessons')
 def lesson_page():
-    return flask.render_template('lessons.html')
+    lesson_pictures = [item for item in os.listdir('static/img/lessons') if item.split('.')[1].lower() in IMG_EXTENSIONS]
+    return flask.render_template('lessons.html',
+                                 files=lesson_pictures)
+
+
+@app.route('/aboutus')
+def aboutus_page():
+    return flask.render_template('instructors.html')
 
 
 @app.route('/boarding')
 def boarding_page():
-    return flask.render_template('boarding.html')
+    boarding_pictures = [item for item in os.listdir('static/img/boarding') if item.split('.')[1].lower() in IMG_EXTENSIONS]
+    # maximum 4 pictures for modal
+    boarding_pictures = boarding_pictures[:4]
+    return flask.render_template('boarding.html',
+                                 files=boarding_pictures)
 
 
-@app.route('/training')
-def training_page():
-    return flask.render_template('training.html')
-
-
-@app.route('/instructors')
-def instructors_page():
-    return flask.render_template('instructors.html')
+@app.route('/shows')
+def shows_page():
+    show_pictures = [item for item in os.listdir('static/img/shows') if item.split('.')[1].lower() in IMG_EXTENSIONS]
+    return flask.render_template('shows.html',
+                                 files=show_pictures)
 
 
 @app.route('/summercamp')
 def camp_page():
-    return flask.render_template('summercamp.html')
+    camp_pictures = [item for item in os.listdir('static/img/camp') if item.split('.')[1].lower() in IMG_EXTENSIONS]
+    return flask.render_template('summercamp.html',
+                                 files=camp_pictures)
 
 
-@app.route('/photos')
+@app.route('/piedmont')
 def photos_page():
-    return flask.render_template('photos.html')
+    return flask.render_template('piedmont.html')
 
 
-@app.route('/contact', methods=['GET'])
+@app.route('/contact', methods=['GET', 'POST'])
 def contact_page():
-    return flask.render_template('contact.html')
+    form = MessageForm()
+    if flask.request.method == 'GET':
+        return flask.render_template('contact.html', form=form)
+
+    elif flask.request.method == 'POST':
+        if form.validate_on_submit():
+            sender_name = form.name.data
+            sender_email = form.email.data
+            subject = form.subject.data
+            message = form.message.data
+            return flask.redirect('/')
+        return flask.render_template('contact.html', form=form)
 
 
 if __name__ == '__main__':
